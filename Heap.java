26ef63d7b6a699a75f8d71ba15338290db33601f
Engin Bozkurt <30608533+enginBozkurt@users.noreply.github.com>
2018-10-24 23:49:56 +0300

    heap sort with custom heap class

---
+++ Heap.java
@@ -0,0 +1,104 @@
+package structs;
+
+public class Heap <E extends Comparable<E>> {
+	
+	private java.util.ArrayList<E> list = new java.util.ArrayList<>();
+	
+	/** Create a default heap */
+	public Heap() {
+	}
+	
+	/** Create a heap from an array of objects */
+	
+	public Heap(E[] objects) {
+		for (int i = 0; i < objects.length; i++)
+			add(objects[i]);	
+	}
+	
+	/** Add a new object into the heap */
+	// appends the object to the tree and then
+	// swaps the object with its parent if the object is greater than its parent. s
+	// This process continue until the new object becomes the root or is not greater than its parent.
+	
+	public void add(E newObject) { 
+		list.add(newObject); // Append to the heap
+		int currentIndex = list.size() - 1; // The index of the last node
+		
+		while (currentIndex > 0) { 
+			
+			int parentIndex = (currentIndex - 1) / 2;
+			// Swap if the current object is greater than its parent
+			
+			if (list.get(currentIndex).compareTo(
+					list.get(parentIndex)) > 0) {
+				
+				E temp = list.get(currentIndex);
+				list.set(currentIndex, list.get(parentIndex));
+				list.set(parentIndex, temp);	
+			}
+			else 
+				break; // The tree is a heap now
+			
+			currentIndex = parentIndex;
+			
+		}
+	}
+		
+		/** Remove the root from the heap */
+		// the method moves the last object to the root position and swaps it with its larger child 
+		// if it is less than the larger child. This process continues until the last object becomes a
+		// leaf or is not less than its children.
+		public E remove() { 
+			if (list.size() == 0) return null;
+			
+			E removedObject = list.get(0);
+			list.set(0, list.get(list.size() - 1));   //move the last object to the root position
+			list.remove(list.size() - 1);             // remove last object
+			
+			
+			int currentIndex = 0;  //current index is root
+			
+			while (currentIndex < list.size()) { 
+				
+				int leftChildIndex = 2 * currentIndex + 1;
+				int rightChildIndex = 2 * currentIndex + 2;
+				
+				// Find the maximum between two children
+				if (leftChildIndex >= list.size()) break; // The tree is a heap
+				
+				int maxIndex = leftChildIndex;
+				
+				if (rightChildIndex < list.size()) {      //compare two children
+					if (list.get(maxIndex).compareTo(
+							list.get(rightChildIndex)) < 0) { 
+						maxIndex = rightChildIndex;	
+					}
+				}
+				
+				// Swap if the current node is less than the maximum
+				
+				if (list.get(currentIndex).compareTo(
+						list.get(maxIndex)) < 0) {
+					
+					E temp = list.get(maxIndex);
+					list.set(maxIndex, list.get(currentIndex)); // swap with the larger child
+					list.set(currentIndex, temp);
+					currentIndex = maxIndex;
+				}
+				
+				else
+					break; // The tree is a heap
+				
+			}
+				
+			return removedObject;	
+				
+			}
+		
+		/** Get the number of nodes in the tree */
+		public int getSize() {
+			return list.size();
+		}
+					
+}
+		
